6982a05e67682a0fc6e4a8b4bf9118385a471bc4
Jonathan Llewellyn <jonathan.llewellyn27@gmail.com>
2021-10-08 10:44:00 -0600

    tests 1st draft

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -15,6 +15,32 @@ afterAll(async () => {
 test('I can start a game', async () => {
 
     let button = await (await driver).findElement(By.id('start-game'));
+    await driver.sleep(1000);
     await button.click();
+    await driver.sleep(1000);
     
 });
+
+test('Clicking the upper left square adds an X to the square', async () => {
+    let cellZero = await driver.findElement(By.id('cell-0'))
+
+    await driver.sleep(1500);
+    await cellZero.click();
+    await driver.sleep(1500);
+})
+
+test('Clicking the upper right square adds an X to the square', async () => {
+    let cellTwo = await driver.findElement(By.id('cell-2'))
+
+    await driver.sleep(1500);
+    await cellTwo.click();
+    await driver.sleep(1500);
+})
+
+test('Clicking the lower right square adds an X to the square', async () => {
+    let cellEight = await driver.findElement(By.id('cell-8'))
+
+    await driver.sleep(1500);
+    await cellEight.click();
+    await driver.sleep(1500);
+})
